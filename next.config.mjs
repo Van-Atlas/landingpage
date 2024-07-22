4c1fe35538e8262e6cfa6a7bddbdc467af842925
Tom <tom.lastbutnotleast@gmail.com>
2024-07-22 22:03:44 +0200

    set static output

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["randomuser.me", "pbs.twimg.com"],
-  },
+  output: "export",
 };
 
 export default nextConfig;
